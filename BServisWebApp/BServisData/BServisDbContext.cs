916d7df69e8661e24d4fca08a327f9db39be5f30
truchanm <milantru1@gmail.com>
2022-03-19 17:53:47 +0100

    Create the db context

---
+++ BServisWebApp/BServisData/BServisDbContext.cs
@@ -0,0 +1,45 @@
+﻿using BServisData.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BServisData
+{
+	public class BServisDbContext : DbContext
+	{
+		public BServisDbContext(DbContextOptions<BServisDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<AdditionalEquipment> AdditionalEquipments { get; set; } = null!;
+		public DbSet<AdditionalEquipmentPhoto> AdditionalEquipmentPhotos { get; set; } = null!;
+		public DbSet<Administrator> Administrators { get; set; } = null!;
+		public DbSet<AuctionBidder> AuctionBidders { get; set; } = null!;
+		public DbSet<AuctionOffer> AuctionOffers { get; set; } = null!;
+		public DbSet<Customer> Customers { get; set; } = null!;
+		public DbSet<Excavator> Excavators { get; set; } = null!;
+		public DbSet<ExcavatorPhoto> ExcavatorPhotos { get; set; } = null!;
+		public DbSet<SkidSteerLoader> SkidSteerLoaders { get; set; } = null!;
+		public DbSet<SparePart> SpareParts { get; set; } = null!;
+		public DbSet<TrackedExcavator> TrackedExcavators { get; set; } = null!;
+		public DbSet<TrackedLoader> TrackedLoaders { get; set; } = null!;
+		public DbSet<User> Users { get; set; } = null!;
+	}
+
+	public class BServisDbContextFactory : IDesignTimeDbContextFactory<BServisDbContext>
+	{
+		public BServisDbContext CreateDbContext(string[] args)
+		{
+			var optionsBuilder = new DbContextOptionsBuilder<BServisDbContext>();
+
+			optionsBuilder.UseMySQL("Data Source = test.db"); // @"Server=(localdb)\mssqllocaldb;Database=Test"
+
+			return new BServisDbContext(optionsBuilder.Options);
+		}
+	}
+}
