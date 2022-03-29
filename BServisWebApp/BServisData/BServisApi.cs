38314b570a4996cce5115ef442bd1d6cfb01a8bb
truchanm <milantru1@gmail.com>
2022-03-29 11:40:26 +0200

    Implement api delete operations

--- BServisWebApp/BServisData/BServisApi.cs
+++ BServisWebApp/BServisData/BServisApi.cs
@@ -1,5 +1,6 @@
 ﻿using BServisData.Interfaces;
 using BServisData.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,152 +11,88 @@ namespace BServisData
 {
 	public class BServisApi : IBServisApi
 	{
-		public Task AuctionOfferHasEnded()
+		IDbContextFactory<BServisDbContext> factory;
+
+		public BServisApi(IDbContextFactory<BServisDbContext> factory)
+		{
+			this.factory = factory;
+		}
+
+		private async Task DeleteItem(IItem item)
+		{
+			using var context = factory.CreateDbContext();
+
+			context.Remove(item);
+
+			await context.SaveChangesAsync();
+		}
+
+		// Create/Update
+		public Task<SkidSteerLoader> SaveSkidSteerLoaderAsync(SkidSteerLoader skidSteerLoader)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteAdditionalEquipmentAsync(AdditionalEquipment additionalEquipment)
+		public Task<TrackedExcavator> SaveTrackedExcavatorAsync(TrackedExcavator trackedExcavator)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteAdditionalEquipmentPhotoAsync(AdditionalEquipmentPhoto additionalEquipmentPhoto)
+		public Task<TrackedLoader> SaveTrackedLoaderAsync(TrackedLoader trackedLoader)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteAdministratorAsync(Administrator administrator)
+		public Task<ExcavatorPhoto> SaveExcavatorPhotoAsync(ExcavatorPhoto excavatorPhoto)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteAuctionBidAsync(AuctionBid auctionBid)
+		public Task<SparePart> SaveSparePartAsync(SparePart sparePart)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteAuctionOfferAsync(AuctionOffer auctionOffer)
+
+		public Task<AdditionalEquipment> SaveAdditionalEquipmentAsync(AdditionalEquipment additionalEquipment)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteCustomerAsync(Customer customer)
+		public Task<AdditionalEquipmentPhoto> SaveAdditionalEquipmentPhotoAsync(AdditionalEquipmentPhoto additionalEquipmentPhoto)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteExcavatorPhotoAsync(ExcavatorPhoto excavatorPhoto)
+
+		public Task<Customer> SaveCustomerAsync(Customer customer)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteSparePartAsync(SparePart sparePart)
+		public Task<Administrator> SaveAdministratorAsync(Administrator administrator)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteTrackedExcavatorAsync(TrackedExcavator trackedExcavator)
+
+		public Task<AuctionOffer> SaveAuctionOfferAsync(AuctionOffer auctionOffer)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task DeleteTrackedLoaderAsync(TrackedLoader trackedLoader)
+		public Task<AuctionBid> SaveAuctionBidAsync(AuctionBid auctionBid)
 		{
 			throw new NotImplementedException();
 		}
-
-		public Task DeleteTrackedSkidSteerLoaderAsync(SkidSteerLoader skidSteerLoader)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<AdditionalEquipment> GetAdditionalEquipmentAsync(int id)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<List<AdditionalEquipmentPhoto>> GetAdditionalEquipmentPhotosAsync(int additionalEquipmentId)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<List<AdditionalEquipment>> GetAdditionalEquipmentsAsync(int numberOfAdditionalEquipments, int startIndex, string excavatorCategory, string? category = null, string? brand = null)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<AdditionalEquipmentPhoto> GetAdditionalEquipmentTitlePhotoAsync(int additionalEquipmentId)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<Administrator> GetAdministratorAsync(int id)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<Administrator?> GetAdministratorAsync(string username, string password)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<AuctionBid> GetAuctionBidAsync(int id)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<List<AuctionBid>> GetAuctionBidsAsync(int auctionOfferId)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<AuctionOffer> GetAuctionOfferAsync(int id)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<List<AuctionOffer>> GetAuctionOffersAsync(int numberOfAuctionOffers, int startIndex)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<Customer> GetCustomerAsync(int id)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<Customer?> GetCustomerAsync(string username, string password)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<List<ExcavatorPhoto>> GetExcavatorPhotosAsync(int excavatorId)
-		{
-			throw new NotImplementedException();
-		}
-
+		
+		// Read
 		public Task<List<Excavator>> GetExcavatorsAsync(int numberOfExcavators, int startIndex, string? category = null, string? brand = null, string? model = null)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<ExcavatorPhoto> GetExcavatorTitlePhotoAsync(int excavatorId)
-		{
-			throw new NotImplementedException();
-		}
-
 		public Task<SkidSteerLoader> GetSkidSteerLoaderAsync(int id)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<SparePart> GetSparePartAsync(int id)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<List<SparePart>> GetSparePartsAsync(int excavatorId)
 		{
 			throw new NotImplementedException();
 		}
@@ -170,57 +107,150 @@ namespace BServisData
 			throw new NotImplementedException();
 		}
 
-		public Task<AdditionalEquipment> SaveAdditionalEquipmentAsync(AdditionalEquipment additionalEquipment)
+		public Task<List<ExcavatorPhoto>> GetExcavatorPhotosAsync(int excavatorId)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<AdditionalEquipmentPhoto> SaveAdditionalEquipmentPhotoAsync(AdditionalEquipmentPhoto additionalEquipmentPhoto)
+		public Task<ExcavatorPhoto> GetExcavatorTitlePhotoAsync(int excavatorId)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<Administrator> SaveAdministratorAsync(Administrator administrator)
+		public Task<List<SparePart>> GetSparePartsAsync(int excavatorId)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<AuctionBid> SaveAuctionBidAsync(AuctionBid auctionBid)
+		public Task<SparePart> GetSparePartAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<AuctionOffer> SaveAuctionOfferAsync(AuctionOffer auctionOffer)
+
+		public Task<List<AdditionalEquipment>> GetAdditionalEquipmentsAsync(int numberOfAdditionalEquipments, int startIndex, string excavatorCategory, string? category = null, string? brand = null)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<Customer> SaveCustomerAsync(Customer customer)
+		public Task<AdditionalEquipment> GetAdditionalEquipmentAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<ExcavatorPhoto> SaveExcavatorPhotoAsync(ExcavatorPhoto excavatorPhoto)
+		public Task<List<AdditionalEquipmentPhoto>> GetAdditionalEquipmentPhotosAsync(int additionalEquipmentId)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<SkidSteerLoader> SaveSkidSteerLoaderAsync(SkidSteerLoader skidSteerLoader)
+		public Task<AdditionalEquipmentPhoto> GetAdditionalEquipmentTitlePhotoAsync(int additionalEquipmentId)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<SparePart> SaveSparePartAsync(SparePart sparePart)
+
+		public Task<Customer> GetCustomerAsync(int id)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<TrackedExcavator> SaveTrackedExcavatorAsync(TrackedExcavator trackedExcavator)
+		public Task<Customer?> GetCustomerAsync(string username, string password)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<TrackedLoader> SaveTrackedLoaderAsync(TrackedLoader trackedLoader)
+		public Task<Administrator> GetAdministratorAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<Administrator?> GetAdministratorAsync(string username, string password)
+		{
+			throw new NotImplementedException();
+		}
+
+
+		public Task<List<AuctionOffer>> GetAuctionOffersAsync(int numberOfAuctionOffers, int startIndex)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<AuctionOffer> GetAuctionOfferAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<List<AuctionBid>> GetAuctionBidsAsync(int auctionOfferId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<AuctionBid> GetAuctionBidAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		// delete
+		public async Task DeleteTrackedSkidSteerLoaderAsync(SkidSteerLoader skidSteerLoader)
+		{
+			await DeleteItem(skidSteerLoader);
+		}
+
+		public async Task DeleteTrackedExcavatorAsync(TrackedExcavator trackedExcavator)
+		{
+			await DeleteItem(trackedExcavator);
+		}
+
+		public async Task DeleteTrackedLoaderAsync(TrackedLoader trackedLoader)
+		{
+			await DeleteItem(trackedLoader);
+		}
+
+		public async Task DeleteExcavatorPhotoAsync(ExcavatorPhoto excavatorPhoto)
+		{
+			await DeleteItem(excavatorPhoto);
+		}
+
+		public async Task DeleteSparePartAsync(SparePart sparePart)
+		{
+			await DeleteItem(sparePart);
+		}
+
+
+		public async Task DeleteAdditionalEquipmentAsync(AdditionalEquipment additionalEquipment)
+		{
+			await DeleteItem(additionalEquipment);
+		}
+
+		public async Task DeleteAdditionalEquipmentPhotoAsync(AdditionalEquipmentPhoto additionalEquipmentPhoto)
+		{
+			await DeleteItem(additionalEquipmentPhoto);
+		}
+
+
+		public async Task DeleteCustomerAsync(Customer customer)
+		{
+			await DeleteItem(customer);
+		}
+
+		public async Task DeleteAdministratorAsync(Administrator administrator)
+		{
+			await DeleteItem(administrator);
+		}
+
+
+		public async Task DeleteAuctionOfferAsync(AuctionOffer auctionOffer)
+		{
+			await DeleteItem(auctionOffer);
+		}
+
+		public async Task DeleteAuctionBidAsync(AuctionBid auctionBid)
+		{
+			await DeleteItem(auctionBid);
+		}
+
+		// more
+		public Task AuctionOfferHasEnded()
 		{
 			throw new NotImplementedException();
 		}
