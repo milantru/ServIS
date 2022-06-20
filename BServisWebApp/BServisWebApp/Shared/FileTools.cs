66c19962e0cef5defffe989cdffaf21a0f1f89bf
truchanm <milantru1@gmail.com>
2022-06-20 19:10:15 +0200

    Add uploading excavator photos

---
+++ BServisWebApp/BServisWebApp/Shared/FileTools.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BServisWebApp.Shared
+{
+	public static class FileTools
+	{
+		public static async Task<byte[]?> GetDataBytesAsync(this IBrowserFile file)
+		{
+			var memoryStream = await GetMemoryStreamAsync(file);
+
+			var dataBytes = memoryStream.ToArray();
+			
+			return dataBytes;
+		}
+		
+		public static string GetDataUrlBase64String(byte[] data, string format)
+		{
+			return $"data:{format};base64,{Convert.ToBase64String(data)}";
+		}
+
+		private static async Task<MemoryStream> GetMemoryStreamAsync(IBrowserFile file)
+		{
+			var stream = file.OpenReadStream();
+
+			var ms = new MemoryStream();
+			await stream.CopyToAsync(ms);
+
+			return ms;
+		}
+	}
+}
