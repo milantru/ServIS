f476e96b4f792362038f6504469439d55ecfe565
truchanm <milantru1@gmail.com>
2022-06-20 11:00:38 +0200

    Update excavators attributes

--- BServisWebApp/BServisData/Models/Excavator.cs
+++ BServisWebApp/BServisData/Models/Excavator.cs
@@ -7,22 +7,24 @@ namespace BServisData.Models
 	{
 		public int Id { get; set; }
 
-		[MaxLength(40)]
+		[Required, MaxLength(40)]
 		public string Category { get; set; } = null!;
 
-		[MaxLength(30)]
+		[Required, MaxLength(30)]
 		public string Brand { get; set; } = null!;
 
-		[MaxLength(30)]
+		[Required, MaxLength(30)]
 		public string Model { get; set; } = null!;
 
-		[MaxLength(80)]
+		[Required, MaxLength(80)]
 		public string Name { get; set; } = null!;
 
+		[Required]
 		public string Description { get; set; } = null!;
 
 		public DateTime? LastInspection { get; set; }
 
+		[Required]
 		public bool IsNew { get; set; }
 
 		public List<SparePart> SpareParts { get; set; } = null!;
