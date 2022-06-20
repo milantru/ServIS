077d3658c72d828f14fff9f3b7d4e3a901339874
truchanm <milantru1@gmail.com>
2022-06-20 11:07:22 +0200

    Implement create/update methods of the API

--- BServisWebApp/BServisData/BServisApi.cs
+++ BServisWebApp/BServisData/BServisApi.cs
@@ -18,72 +18,294 @@ namespace BServisData
 			this.factory = factory;
 		}
 
-		private async Task DeleteItem(IItem item)
+		// Create/Update
+		public async Task<SkidSteerLoader> SaveSkidSteerLoaderAsync(SkidSteerLoader skidSteerLoader)
 		{
 			using var context = factory.CreateDbContext();
+			SkidSteerLoader currentSkidSteerLoader;
 
-			context.Remove(item);
+			if (skidSteerLoader.Id == 0)
+			{
+				context.Add(skidSteerLoader);
+			}
+			else
+			{
+				currentSkidSteerLoader = await context.SkidSteerLoaders
+					.Include(ssl => ssl.SpareParts)
+					.FirstOrDefaultAsync(ssl => ssl.Id == skidSteerLoader.Id);
+
+				UpdateSkidSteerLoaderData(context, currentSkidSteerLoader, skidSteerLoader);
+			}
 
 			await context.SaveChangesAsync();
+
+			return skidSteerLoader;
 		}
 
-		// Create/Update
-		public Task<SkidSteerLoader> SaveSkidSteerLoaderAsync(SkidSteerLoader skidSteerLoader)
+		public async Task<TrackedExcavator> SaveTrackedExcavatorAsync(TrackedExcavator trackedExcavator)
 		{
-			throw new NotImplementedException();
+			using var context = factory.CreateDbContext();
+			TrackedExcavator currentTrackedExcavator;
+
+			if (trackedExcavator.Id == 0)
+		{
+				context.Add(trackedExcavator);
+		}
+			else
+			{
+				currentTrackedExcavator = await context.TrackedExcavators
+					.Include(te => te.SpareParts)
+					.FirstOrDefaultAsync(te => te.Id == trackedExcavator.Id);
+
+				UpdateTrackedExcavatorData(context, currentTrackedExcavator, trackedExcavator);
+			}
+
+			await context.SaveChangesAsync();
+
+			return trackedExcavator;
 		}
 
-		public Task<TrackedExcavator> SaveTrackedExcavatorAsync(TrackedExcavator trackedExcavator)
+		public async Task<TrackedLoader> SaveTrackedLoaderAsync(TrackedLoader trackedLoader)
 		{
-			throw new NotImplementedException();
+			using var context = factory.CreateDbContext();
+			TrackedLoader currentTrackedLoader;
+
+			if (trackedLoader.Id == 0)
+			{
+				context.Add(trackedLoader);
+		}
+			else
+			{
+				currentTrackedLoader = await context.TrackedLoaders
+					.Include(tl => tl.SpareParts)
+					.FirstOrDefaultAsync(tl => tl.Id == trackedLoader.Id);
+
+				UpdateTrackedLoaderData(context, currentTrackedLoader, trackedLoader);
+			}
+
+			await context.SaveChangesAsync();
+
+			return trackedLoader;
 		}
 
-		public Task<TrackedLoader> SaveTrackedLoaderAsync(TrackedLoader trackedLoader)
+		public async Task<ExcavatorPhoto> SaveExcavatorPhotoAsync(ExcavatorPhoto excavatorPhoto)
 		{
-			throw new NotImplementedException();
+			using var context = factory.CreateDbContext();
+			ExcavatorPhoto currentExcavatorPhoto;
+
+			if (excavatorPhoto.Id == 0)
+			{
+				context.Add(excavatorPhoto);
+		}
+			else
+			{
+				currentExcavatorPhoto = await context.ExcavatorPhotos
+					.Include(ep => ep.Excavator)
+					.FirstOrDefaultAsync(ep => ep.Id == excavatorPhoto.Id);
+
+				currentExcavatorPhoto.Excavator = excavatorPhoto.Excavator;
+				//int excavatorId = excavatorPhoto.Excavator.Id;
+				//currentExcavatorPhoto.Excavator = await context.Excavators
+				//	.FirstOrDefaultAsync(e => e.Id == excavatorId);
+				currentExcavatorPhoto.Photo = excavatorPhoto.Photo;
+				currentExcavatorPhoto.IsTitle = excavatorPhoto.IsTitle;
+			}
+
+			await context.SaveChangesAsync();
+
+			return excavatorPhoto;
 		}
 
-		public Task<ExcavatorPhoto> SaveExcavatorPhotoAsync(ExcavatorPhoto excavatorPhoto)
+		public async Task<SparePart> SaveSparePartAsync(SparePart sparePart)
 		{
-			throw new NotImplementedException();
+			using var context = factory.CreateDbContext();
+			SparePart currentSparePart;
+
+			if (sparePart.Id == 0)
+			{
+				context.Add(sparePart);
 		}
+			else
+			{
+				currentSparePart = await context.SpareParts
+					.Include(sp => sp.Excavators)
+					.FirstOrDefaultAsync(sp => sp.Id == sparePart.Id);
 
-		public Task<SparePart> SaveSparePartAsync(SparePart sparePart)
-		{
-			throw new NotImplementedException();
-		}
+				currentSparePart.CatalogNumber = sparePart.CatalogNumber;
+				currentSparePart.Name = sparePart.Name;
+				//currentSparePart.Excavators = sparePart.Excavators;
+				var ids = sparePart.Excavators.Select(e => e.Id);
+				currentSparePart.Excavators = context.Excavators
+					.Where(e => ids.Contains(e.Id))
+					.ToList();
+			}
 
+			await context.SaveChangesAsync();
 
-		public Task<AdditionalEquipment> SaveAdditionalEquipmentAsync(AdditionalEquipment additionalEquipment)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<AdditionalEquipmentPhoto> SaveAdditionalEquipmentPhotoAsync(AdditionalEquipmentPhoto additionalEquipmentPhoto)
-		{
-			throw new NotImplementedException();
-		}
-
-
-		public Task<Customer> SaveCustomerAsync(Customer customer)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<Administrator> SaveAdministratorAsync(Administrator administrator)
-		{
-			throw new NotImplementedException();
+			return sparePart;
 		}
 
 
-		public Task<AuctionOffer> SaveAuctionOfferAsync(AuctionOffer auctionOffer)
+		public async Task<AdditionalEquipment> SaveAdditionalEquipmentAsync(AdditionalEquipment additionalEquipment)
 		{
-			throw new NotImplementedException();
+			using var context = factory.CreateDbContext();
+			AdditionalEquipment currentAdditionalEquipment;
+
+			if (additionalEquipment.Id == 0)
+			{
+				context.Add(additionalEquipment);
+		}
+			else
+			{
+				currentAdditionalEquipment = await context.AdditionalEquipments
+					.FirstOrDefaultAsync(ae => ae.Id == additionalEquipment.Id);
+
+				currentAdditionalEquipment.ForWhichExcavatorCategory = additionalEquipment.ForWhichExcavatorCategory;
+				currentAdditionalEquipment.Category = additionalEquipment.Category;
+				currentAdditionalEquipment.Brand = additionalEquipment.Brand;
+				currentAdditionalEquipment.Name = additionalEquipment.Name;
+				currentAdditionalEquipment.Description = additionalEquipment.Description;
+				currentAdditionalEquipment.Price = additionalEquipment.Price;
+			}
+
+			await context.SaveChangesAsync();
+
+			return additionalEquipment;
 		}
 
-		public Task<AuctionBid> SaveAuctionBidAsync(AuctionBid auctionBid)
+		public async Task<AdditionalEquipmentPhoto> SaveAdditionalEquipmentPhotoAsync(AdditionalEquipmentPhoto additionalEquipmentPhoto)
 		{
-			throw new NotImplementedException();
+			using var context = factory.CreateDbContext();
+			AdditionalEquipmentPhoto currentAdditionalEquipmentPhoto;
+
+			if (additionalEquipmentPhoto.Id == 0)
+			{
+				context.Add(additionalEquipmentPhoto);
+		}
+			else
+			{
+				currentAdditionalEquipmentPhoto = await context.AdditionalEquipmentPhotos
+					.Include(aep => aep.AdditionalEquipment)
+					.FirstOrDefaultAsync(aep => aep.Id == additionalEquipmentPhoto.Id);
+
+				currentAdditionalEquipmentPhoto.AdditionalEquipment = additionalEquipmentPhoto.AdditionalEquipment;
+				//int additionalEquipmentPhotoId = additionalEquipmentPhoto.AdditionalEquipment.Id;
+				//currentAdditionalEquipmentPhoto.AdditionalEquipment = await context.AdditionalEquipments
+				//	.FirstOrDefaultAsync(ae => ae.Id == additionalEquipmentPhotoId);
+				currentAdditionalEquipmentPhoto.Photo = additionalEquipmentPhoto.Photo;
+				currentAdditionalEquipmentPhoto.IsTitle = additionalEquipmentPhoto.IsTitle;
+			}
+
+			await context.SaveChangesAsync();
+
+			return additionalEquipmentPhoto;
+		}
+
+
+		public async Task<Customer> SaveCustomerAsync(Customer customer)
+		{
+			using var context = factory.CreateDbContext();
+			Customer currentCustomer;
+
+			if (customer.Id == 0)
+			{
+				context.Add(customer);
+		}
+			else
+			{
+				currentCustomer = await context.Customers
+					.FirstOrDefaultAsync(c => c.Id == customer.Id);
+
+				UpdateCustomerData(context, currentCustomer, customer);
+			}
+
+			await context.SaveChangesAsync();
+
+			return customer;
+		}
+
+		public async Task<Administrator> SaveAdministratorAsync(Administrator administrator)
+		{
+			using var context = factory.CreateDbContext();
+			Administrator currentAdministrator;
+
+			if (administrator.Id == 0)
+			{
+				context.Add(administrator);
+		}
+			else
+			{
+				currentAdministrator = await context.Administrators
+					.FirstOrDefaultAsync(a => a.Id == administrator.Id);
+
+				UpdateAdministratorData(context, currentAdministrator, administrator);
+			}
+
+			await context.SaveChangesAsync();
+
+			return administrator;
+		}
+
+
+		public async Task<AuctionOffer> SaveAuctionOfferAsync(AuctionOffer auctionOffer)
+		{
+			using var context = factory.CreateDbContext();
+			AuctionOffer currentAuctionOffer;
+
+			if (auctionOffer.Id == 0)
+			{
+				context.Add(auctionOffer);
+		}
+			else
+			{
+				currentAuctionOffer = await context.AuctionOffers
+					.Include(ao => ao.Excavator)
+					.FirstOrDefaultAsync(ao => ao.Id == auctionOffer.Id);
+
+				currentAuctionOffer.Excavator = auctionOffer.Excavator;
+				//int excavatorId = auctionOffer.Excavator.Id;
+				//currentAuctionOffer.Excavator = await context.Excavators
+				//	.FirstOrDefaultAsync(e => e.Id == excavatorId);
+				currentAuctionOffer.Description = auctionOffer.Description;
+				currentAuctionOffer.OfferEnd = auctionOffer.OfferEnd;
+				currentAuctionOffer.StartingBid = auctionOffer.StartingBid;
+			}
+
+			await context.SaveChangesAsync();
+
+			return auctionOffer;
+		}
+
+		public async Task<AuctionBid> SaveAuctionBidAsync(AuctionBid auctionBid)
+		{
+			using var context = factory.CreateDbContext();
+			AuctionBid currentAuctionBid;
+
+			if (auctionBid.Id == 0)
+			{
+				context.Add(auctionBid);
+		}
+			else
+			{
+				currentAuctionBid = await context.AuctionBids
+					.Include(ab => ab.User)
+					.Include(ab => ab.AuctionOffer)
+					.FirstOrDefaultAsync(ab => ab.Id == auctionBid.Id);
+		
+				currentAuctionBid.User = auctionBid.User;
+				//int userId = auctionBid.User.Id;
+				//currentAuctionBid.User = await context.Users
+				//	.FirstOrDefaultAsync(u => u.Id == userId);
+				currentAuctionBid.AuctionOffer = auctionBid.AuctionOffer;
+				//int auctionOfferId = auctionBid.AuctionOffer.Id;
+				//currentAuctionBid.AuctionOffer = await context.AuctionOffers
+				//	.FirstOrDefaultAsync(ao => ao.Id == auctionOfferId);
+				currentAuctionBid.Bid = auctionBid.Bid;
+			}
+
+			await context.SaveChangesAsync();
+
+			return auctionBid;
 		}
 		
 		// Read
