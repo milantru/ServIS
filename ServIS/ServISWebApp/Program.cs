ecc8a8b79f3586b067e4a5d83aeed857f09a8715
truchanm <milantru1@gmail.com>
2022-07-12 21:00:07 +0200

    Register services

--- ServIS/ServISWebApp/Program.cs
+++ ServIS/ServISWebApp/Program.cs
@@ -3,12 +3,18 @@ using ServISData.Interfaces;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using ServISWebApp.Auth;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddAuthenticationCore();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddScoped<ProtectedSessionStorage>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddDbContextFactory<ServISDbContext>(options =>
 	{
 		var connectionString = ServISDbContextFactory.GetConnectionString();
