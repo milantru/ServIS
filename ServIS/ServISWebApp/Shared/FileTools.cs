1ae7186a5df1e76c05de3c6c0ddeb089847049f8
truchanm <milantru1@gmail.com>
2022-07-18 15:42:11 +0200

    Add alert notifying user if photo is invalid

--- ServIS/ServISWebApp/Shared/FileTools.cs
+++ ServIS/ServISWebApp/Shared/FileTools.cs
@@ -9,13 +9,37 @@ namespace ServISWebApp.Shared
 			var memoryStream = await GetMemoryStreamAsync(file);
 
 			var dataBytes = memoryStream.ToArray();
-			
+
 			return dataBytes;
 		}
-		
+
 		public static string GetDataUrlBase64String(byte[] data, string format)
 		{
 			return $"data:{format};base64,{Convert.ToBase64String(data)}";
+		}
+
+		public static bool IsValidImageFile(this IBrowserFile imageFile, out string? errorMessage)
+		{
+			if (imageFile == null)
+			{
+				errorMessage = "Chýba súbor"; // missing image file
+				return false;
+			}
+			else if (imageFile.ContentType != "image/jpeg")
+			{
+				errorMessage = "Nepodporovaný typ súboru"; // unsupported file type
+				return false;
+			}
+			else if (imageFile.Size > 50_000)
+			{
+				errorMessage = "Súbor je príliš veľký"; // file is too large
+				return false;
+			}
+			else
+			{
+				errorMessage = null;
+				return true;
+			}
 		}
 
 		private static async Task<MemoryStream> GetMemoryStreamAsync(IBrowserFile file)
