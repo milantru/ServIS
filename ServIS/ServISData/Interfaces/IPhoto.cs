e43c257a217d016224e8fd33c7b422f273a44159
Milan <milantru1@gmail.com>
2022-08-13 18:51:24 +0200

    Add IPhoto interface

---
+++ ServIS/ServISData/Interfaces/IPhoto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServISData.Interfaces
+{
+	public interface IPhoto
+	{
+		public byte[] Photo { get; }
+		public bool IsTitle { get; }
+	}
+}
