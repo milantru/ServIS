5d5901193b1759d424d6abdeeca14e58fca6d0ec
Milan <milantru1@gmail.com>
2022-09-03 15:12:04 +0200

    Add ItemsManagement component

---
+++ ServIS/ServISWebApp/Components/Managements/ItemsManagement.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ServISWebApp.Components.Managements
+{
+	public abstract class ItemsManagement<ItemType> : ComponentBase where ItemType : class, new()
+	{
+		protected ItemsTableLister<ItemType> itemsLister = null!;
+
+		public ItemType SelectedItem { get; protected set; } = new();
+
+		public async Task ReloadItemsAsync()
+		{
+			await itemsLister.ReloadItemsAsync();
+		}
+	}
+}
