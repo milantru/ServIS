3efa7e48e873223a842931b613c7b784a01e7977
truchanm <milantru1@gmail.com>
2023-03-01 19:06:04 +0100

    Add Email class

---
+++ ServIS/ServISWebApp/Shared/Email.cs
@@ -0,0 +1,20 @@
+﻿using MailKit;
+using MimeKit;
+
+namespace ServISWebApp.Shared
+{
+	public class Email
+	{
+		public UniqueId Uid { get; set; }
+		public string MessageId { get; set; } = null!;
+		public MessageIdList References { get; set; } = null!;
+		public string FromName { get; set; } = null!;
+		public string FromAddress { get; set; } = null!;
+		public string ToName { get; set; } = null!;
+		public string ToAddress { get; set; } = null!;
+		public string Subject { get; set; } = null!;
+		public string Text { get; set; } = null!;
+		public bool Read { get; set; }
+		public DateTime DateTime { get; set; }
+	}
+}
