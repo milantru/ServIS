05a8cf3e3e51012f61016faf26475efb0b746f65
truchanm <milantru1@gmail.com>
2023-05-17 18:28:32 +0200

    Refactor

--- ServIS/ServISWebApp/CssProviders/CustomCssClassProvider.cs
+++ ServIS/ServISWebApp/CssProviders/CustomCssClassProvider.cs
@@ -5,22 +5,12 @@ namespace ServISWebApp.CssProviders
 {
     public class CustomCssClassProvider<ProviderType> : ComponentBase where ProviderType : FieldCssClassProvider, new()
     {
-        [CascadingParameter]
-        EditContext? CurrentEditContext { get; set; }
+        [EditorRequired, CascadingParameter]
+        EditContext CurrentEditContext { get; set; } = null!;
         public ProviderType Provider { get; set; } = new ProviderType();
 
         protected override void OnInitialized()
         {
-            if (CurrentEditContext == null)
-            {
-                throw new InvalidOperationException
-                    (
-                        $"{nameof(DataAnnotationsValidator)} requires a cascading " +
-                        $"parameter of type {nameof(EditContext)}. " +
-                        $"For example, you can use {nameof(DataAnnotationsValidator)} " +
-                        "inside an EditForm."
-                    );
-            }
             CurrentEditContext.SetFieldCssClassProvider(Provider);
         }
     }
